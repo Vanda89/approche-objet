90ab5ca417f2dd83b4e5121e18acbfc790fb64ec
Sandrine ALCAZAR <33418628+Vanda89@users.noreply.github.com>
2025-07-15 12:45:12 +0200

    feat(TP3): Update TP2 by using getter and setter to Person

--- src/main/java/entites/TestPerson.java
+++ src/main/java/entites/TestPerson.java
@@ -4,18 +4,19 @@ import entites2.Person;
 
 public class TestPerson {
     public static void main(String[] args) {
-        Person pers1 = new Person("Georges", "Dupont");
+        Person pers1 = new Person(
+                "Dupont","Georges");
 
         MailingAddress adr2 =
                 new MailingAddress(2, "GrandRue", 30000, "Nîmes");
         Person pers2 = new Person(
-                "Françoise", "Durand", adr2);
+                 "Durand", "Françoise");
+        pers2.setAddress(adr2);
 
-        System.out.println(pers1.firstName + " " + pers1.lastName);
-
-        System.out.println(pers2.firstName + " " + pers2.lastName + ", " +
-                pers2.address.streetNumber + " " + pers2.address.streetLabel + ", " +
-                pers2.address.postalCode + " " + pers2.address.city);
+        pers2.displayName();
+        System.out.println(pers2.getAddress().streetNumber + " " + pers2.getAddress().streetLabel + ", " +
+                pers2.getAddress().postalCode +
+                " " + pers2.getAddress().city);
 
     }
 }
--- src/main/java/entites2/Person.java
+++ src/main/java/entites2/Person.java
@@ -3,20 +3,46 @@ package entites2;
 import entites.MailingAddress;
 
 public class Person {
-    public String firstName;
     public String lastName;
+    public String firstName;
     public MailingAddress address;
 
-    public Person(String firstName, String lastName) {
+    public Person(String lastName, String firstName) {
+        this.lastName = lastName;
         this.firstName = firstName;
+    }
+    public Person(
+                   String lastName,
+                   String firstName,
+                  MailingAddress address) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.address = address;
+    }
+
+    public void displayName()  {
+        System.out.println(this.lastName +
+                " " + this.firstName);
+    }
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    public Person(String firstName,
-                   String lastName,
-                  MailingAddress address) {
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
-        this.lastName = lastName;
+    }
+    public void setAddress(MailingAddress address) {
         this.address = address;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public MailingAddress getAddress() {
+        return this.address;
     }
 
 
